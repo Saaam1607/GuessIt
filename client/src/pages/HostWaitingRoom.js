fd83de1d409aa1758a7c1085e9fc094227283299
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2024-01-02 23:31:47 +0100

    fixes

--- client/src/pages/HostWaitingRoom.js
+++ client/src/pages/HostWaitingRoom.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
 
-const socket = io.connect("http://localhost:8000");
+const socket = io.connect("https://guessitserver.onrender.com");
 
 
 
